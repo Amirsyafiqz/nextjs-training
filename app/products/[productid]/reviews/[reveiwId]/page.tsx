a911e27e2be769f4b5c1b4a632c4623bba3a557f
Amirsyafiqz <amirsyafiq2109@gmail.com>
2025-08-22 18:28:19 +0800

    docs: update learning progress (20-21 Aug) - studied dynamic routes, debugging productId & reviewId

---
+++ app/products/[productid]/reviews/[reveiwId]/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductReview({ params }: { params: { productId: string; reviewId: string } }) {
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}
+    </h1>
+  );
+}
+
+
